817eae4f7a3e8d47acf9cf9a50132393057a0eea
Joey Glocke <jglockstock@gmail.com>
2012-10-23 23:21:42 -0500

    Continued design, added js layout file

---
+++ app/assets/javascripts/layout.js
@@ -0,0 +1,37 @@
+var Welcome = function() {
+	/**
+	*	Private Methods (separated just by closing brace)
+	*/
+
+	return {
+		/**
+		*	Public Methods (separated by commas)
+		*/
+		documentReady: function() {
+			$('#tv').prop('checked', true);
+			
+			$('#tv').click(function() {
+				if ($(this).is(':checked')) {
+					// checkbox is now being checked
+					$('#tv_section').slideDown('slow');
+				} else {
+					// checkbox is now being unchecked
+					$('#tv_section').slideUp('slow');
+				}
+			});
+
+			$(".poster").hover(function(){
+				if ($(this).data("bouncing") == false || $(this).data("bouncing") == undefined){
+				    $(this).effect("bounce", { distance: 5, times: 1 }, 500);
+				    $(this).data("bouncing", true);
+				}
+			},function () {
+			   	$(this).data("bouncing", false);
+			});
+		}
+	};
+}();
+
+$(document).ready(function() {
+	Welcome.documentReady();
+});
