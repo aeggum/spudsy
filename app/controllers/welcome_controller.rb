738a6fe2693c422ca41a9c1e58a32a0ccd35fd63
Adam Eggum <aeggum12@gmail.com>
2012-10-26 00:23:28 -0500

    Looking at pushing this to heroku.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,7 @@ require "rotten"
 class WelcomeController < ApplicationController
   Rotten.api_key = 'pykjuv5y44fywgpu2m7rt4dk'
   
-  def index
+  def index 
    
     # Doing some testing with the RT API wrapper
     argo = Rotten::Movie.find_first "Argo"
