62dc95c89a0395ef70fbe09753b967156eaf4124
Adam Eggum <aeggum12@gmail.com>
2012-10-26 00:36:13 -0500

    troubleshooting continues.

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,8 @@ end
 
 module DemoApp
   class Application < Rails::Application
+    
+    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
