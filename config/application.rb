92b51aa3bc93df79c2b9458b450d24125853d859
Adam Eggum <aeggum12@gmail.com>
2012-10-28 20:31:43 -0500

    test

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,7 @@ module DemoApp
   class Application < Rails::Application
     
     config.assets.initialize_on_precompile = false
+    config.assets.precompile += ['960.css']
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
