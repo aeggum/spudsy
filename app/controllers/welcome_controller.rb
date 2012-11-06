c9657343135ff8ba1e4ed605f331bbc3f73a80c0
Adam Eggum <aeggum12@gmail.com>
2012-11-05 21:53:51 -0600

    Added tv api calls for main page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,11 +1,13 @@
 require "rotten"
 require "tmdb"
+require 'amazon/ecs'
 
 class WelcomeController < ApplicationController
   Rotten.api_key = 'pykjuv5y44fywgpu2m7rt4dk'
   Tmdb::Tmdb.api_key = "8da8a86a8b272a70d20c08a35b576d50"
   Tmdb::Tmdb.default_language = "en"
   
+  caches_action :get_shows
   def index 
    
     # Doing some testing with the RT API wrapper
@@ -88,6 +90,9 @@ class WelcomeController < ApplicationController
     @mummy_score = mummy.ratings['critics_score']
     @mummy_poster = mummy.posters['detailed']
     
+    @show_array = get_shows
+
+    
     
     # will print out a giant blob in the console
     # @movie = Tmdb::TmdbMovie.find :title => "Argo", :limit => 1
@@ -111,5 +116,36 @@ class WelcomeController < ApplicationController
   
   def details
     # This is a placeholder - users will be redirected to it when they are signed in, for now
+    # alternatively,
+    Amazon::Ecs.configure do |options|
+      options[:associate_tag] = 'Spudsy Devs'
+      options[:AWS_access_key_id] = 'AKIAIKFAMUN6RODAOVKA'
+      options[:AWS_secret_key] = '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'
+    end
+
+    # options provided on method call will merge with the default options
+    res = Amazon::Ecs.item_search('Seinfeld', {:response_group => 'Medium', :sort => 'salesrank'})
+    @res = res
+    @total_pages = @res.total_pages
+    @first = @res.items.first
   end
+  
+  private 
+  
+    def get_shows
+      tvdb = TvdbParty::Search.new("FACBC9B54A326107")
+    
+      tv_show_titles = ['Seinfeld', 'The West Wing', 'Person of Interest', 'The Walking Dead', 'Family Guy', '24', 'Dexter', 'Breaking Bad']
+      show_array = Array.new
+      tv_show_titles.each { |show|
+        hash_show = Hash.new
+        hash_show['title'] = show
+        tvdb_show = tvdb.get_series_by_id(tvdb.search(show)[0]["seriesid"])
+        hash_show['rating'] = tvdb_show.rating
+        hash_show['poster'] = tvdb_show.posters('en').first.url
+        show_array.push(hash_show)
+      }
+    
+      return show_array
+    end
 end
