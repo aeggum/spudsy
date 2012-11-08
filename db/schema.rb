2076a5c22e09ed35db4ea31c8fae4e3c9e7686a0
Justin Smith <Justin@justin-smiths-macbook.local>
2012-11-08 17:42:25 -0600

    Committing some shit

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121108071034) do
+ActiveRecord::Schema.define(:version => 20121108061423) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -63,7 +63,6 @@ ActiveRecord::Schema.define(:version => 20121108071034) do
     t.datetime "updated_at",   :null => false
     t.float    "user_rating"
     t.string   "mpaa_rating"
-    t.string   "poster"
   end
 
   create_table "tv_shows", :force => true do |t|
