58a592db10dd60716dfc378a4985c968d7305e8a
Adam Eggum <aeggum12@gmail.com>
2012-11-08 01:39:35 -0600

    Removed the 30 API calls and added seeds file to get the api calls into the database.
    Must run rake db:seed

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121101044548) do
+ActiveRecord::Schema.define(:version => 20121108071034) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -63,6 +63,7 @@ ActiveRecord::Schema.define(:version => 20121101044548) do
     t.datetime "updated_at",   :null => false
     t.float    "user_rating"
     t.string   "mpaa_rating"
+    t.string   "poster"
   end
 
   create_table "tv_shows", :force => true do |t|
@@ -71,6 +72,7 @@ ActiveRecord::Schema.define(:version => 20121101044548) do
     t.text     "description"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+    t.string   "photo_url"
   end
 
   create_table "users", :force => true do |t|
