86cdd2e739c47b748f36a8ab7d447610fbc95643
Adam Eggum <aeggum12@gmail.com>
2012-11-15 00:29:55 -0600

    You will need to run the following once you pull this:
    rake db:drop db:create db:migrate db:seed

---
+++ db/migrate/20121031051943_create_movies.rb
@@ -0,0 +1,17 @@
+class CreateMovies < ActiveRecord::Migration
+  def change
+    create_table :movies do |t|
+      t.string :name
+      t.float :rating
+      t.text :description
+      t.date :release_date
+      t.float :user_rating
+      t.string :mpaa_rating
+      t.string :poster
+
+      t.timestamps
+    end
+    
+    add_index :movies, :name
+  end
+end
