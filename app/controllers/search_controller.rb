2c09d43397f5be4a998a12ee4bcc738dea8ae1c7
Justin Smith <Justin@justin-smiths-macbook.local>
2012-11-28 18:01:26 -0600

    Fixed search box css

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,7 +3,7 @@ class SearchController < ApplicationController
   def results
     #@movie_results = Movie.text_search(params[:query]) 
     #@tv_results = TvShow.text_search(params[:query])
-    @search_results = PgSearch.multisearch(params[:query])
+    @search_results = PgSearch.multisearch(params[:search_box])
     #@actor_results = Actor.text_search(params[:query])
     
   end
