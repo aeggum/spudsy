757fa1ca5fbad7dc91877a71c7fae0c8ed6ced4c
Joey Glocke <jglockstock@gmail.com>
2012-11-28 23:37:41 -0600

    i dunno

--- db/schema.rb
+++ db/schema.rb
@@ -64,7 +64,7 @@ ActiveRecord::Schema.define(:version => 20121115054532) do
     t.string   "mpaa_rating"
     t.string   "poster"
     t.integer  "runtime"
-    t.integer  "rt_id"
+    t.string   "rt_id"
     t.datetime "created_at",   :null => false
     t.datetime "updated_at",   :null => false
   end
