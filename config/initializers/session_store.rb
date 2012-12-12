8a2cdb66a55e0c4e2cce9296d1e52e2ce76d36cc
Adam Eggum <aeggum12@gmail.com>
2012-12-11 23:38:57 -0600

    updated movies.txt

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,17 @@
 # Be sure to restart your server when you modify this file.
 
-DemoApp::Application.config.session_store :cookie_store, :key => '_demo_app_session'
+#DemoApp::Application.config.session_store :cookie_store, :key => '_demo_app_session'
 
+DemoApp::Application.config.session_store :active_record_store
+
+module ActionController
+  module Flash
+    class FlashHash < Hash
+      def method_missing(m, *a, &b)
+      end
+    end
+  end
+end
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rails generate session_migration")
