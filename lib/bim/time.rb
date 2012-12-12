1fd94d21ddd5562b2e6a403502cd9838d525dbc8
Adam Eggum <aeggum12@gmail.com>
2012-12-12 00:46:13 -0600

    Updated Time class to make time calls a little bit easier.

--- lib/bim/time.rb
+++ lib/bim/time.rb
@@ -1,4 +1,4 @@
-
+# Updates to the Time class
 class Time
   def round(seconds = 60)
     Time.at((self.to_f / seconds).round * seconds)
@@ -6,5 +6,16 @@ class Time
 
   def floor(seconds = 60)
     Time.at((self.to_f / seconds).floor * seconds)
+  end
+  
+  # call such as Time.now.readable(30.minutes)
+  def readable(seconds = 60) 
+    Time.at((self.to_f / seconds).floor * seconds).strftime("%I:%M%p")
+  end
+  
+  # call such as Time.now.readable_plus(30.minutes, 30)
+  def readable_plus(seconds = 60, plus = 30)
+    t = Time.now.floor(30.minutes) + plus.minutes
+    t.readable(seconds)
   end
 end
