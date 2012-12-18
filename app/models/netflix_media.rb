df9d971d14098110cb9ea927436043e4e8ee22c2
Adam Eggum <aeggum12@gmail.com>
2012-12-18 02:47:08 -0600

    rails runner db/netflix_seeds.rb

--- app/models/netflix_media.rb
+++ app/models/netflix_media.rb
@@ -1,20 +1,29 @@
 class NetflixMedia < ActiveRecord::Base
-  attr_accessible :media_id, :media_type, :name
+  attr_accessible :media_id, :media_type, :name, :spudsy_rating
   before_save :default_values
   
   # Gets the media id for each netflix type before it is saved
   def default_values
     id = -1
-    puts self.name
-    puts self.media_type
+    spudsy_rating = -1
     if (self.media_type == "Movie")
-      id = Movie.where { name == self.name }[0].id
+      m = Movie.where [ 'name = ? ', self.name ]
+      begin
+        id = m[0].id
+        spudsy_rating = m[0].spudsy_rating
+      rescue
+        id = -1
+      end
     elsif (self.media_type == "TvShow")
-      puts self.name
-      puts self.name.class
-      t = TvShow.where [ 'name = ? ', self.name ] #TvShow.where {{ name => self.name }}[0]
-      id = t[0].id
+      t = TvShow.where [ 'name = ? ', self.name ]
+      begin
+        id = t[0].id
+        spudsy_rating = t[0].spudsy_rating
+      rescue
+        id = -1
+      end
     end
     self.media_id = id
+    self.spudsy_rating = spudsy_rating
   end
 end
--- db/migrate/20121218070235_create_netflix_media.rb
+++ db/migrate/20121218070235_create_netflix_media.rb
@@ -4,11 +4,13 @@ class CreateNetflixMedia < ActiveRecord::Migration
       t.string :name
       t.string :media_type
       t.integer :media_id
+      t.integer :spudsy_rating
 
       t.timestamps
     end
     
     add_index :netflix_media, :name
-    add_index :netflix_media, :media_id, :unique => true
+    add_index :netflix_media, :spudsy_rating
+    add_index :netflix_media, ["name", "media_id", "media_type"], :unique => true
   end
 end
